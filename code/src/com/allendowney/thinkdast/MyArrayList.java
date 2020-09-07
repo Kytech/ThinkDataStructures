97fb46b96bcae2700dc027e9278c6eced3288ec5
Kytech <Kytech@users.noreply.github.com>
2020-09-07 16:02:42 -0600

    Completed Exercise 2

--- code/src/com/allendowney/thinkdast/MyArrayList.java
+++ code/src/com/allendowney/thinkdast/MyArrayList.java
@@ -44,8 +44,15 @@ public class MyArrayList<T> implements List<T> {
 
 	@Override
 	public boolean add(T element) {
-		// TODO: FILL THIS IN!
-		return false;
+		if (size >= array.length) {
+			// make a bigger array and copy over the elements
+			T[] bigger = (T[]) new Object[array.length * 2];
+			System.arraycopy(array, 0, bigger, 0, array.length);
+			array = bigger;
+		}
+		array[size] = element;
+		size++;
+		return true;
 	}
 
 	@Override
@@ -110,7 +117,11 @@ public class MyArrayList<T> implements List<T> {
 
 	@Override
 	public int indexOf(Object target) {
-		// TODO: FILL THIS IN!
+		for (int i = 0; i < size; i++) {
+			if (equals(array[i], target)) {
+				return i;
+			}
+		}
 		return -1;
 	}
 
@@ -119,7 +130,7 @@ public class MyArrayList<T> implements List<T> {
 	 * Handles the special case that the target is null.
 	 *
 	 * @param target
-	 * @param object
+	 * @param element
 	 */
 	private boolean equals(Object target, Object element) {
 		if (target == null) {
@@ -181,8 +192,14 @@ public class MyArrayList<T> implements List<T> {
 
 	@Override
 	public T remove(int index) {
-		// TODO: FILL THIS IN!
-		return null;
+		T deledElem = get(index);
+
+		for (int i=index; index<size-1; i++) {
+			array[i] = array[i+1];
+		}
+		size--;
+
+		return deledElem;
 	}
 
 	@Override
@@ -201,8 +218,9 @@ public class MyArrayList<T> implements List<T> {
 
 	@Override
 	public T set(int index, T element) {
-		// TODO: FILL THIS IN!
-		return null;
+		T oldElement = get(index);
+		array[index] = element;
+		return oldElement;
 	}
 
 	@Override
